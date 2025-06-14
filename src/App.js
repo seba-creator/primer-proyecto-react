59bcb3ead6020329e0f4fa2eef253613ad8040fa
sarocenaP <sarocena@polotab.com>
2025-06-14 16:15:32 -0300

    fondo Particles, hero y sectionFloat

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
 import './App.css';
 
-import './components/Navbar'
-import Navbar from './components/Navbar';
+import Navbar from './components/Navbar-file/Navbar';
+import BgParticles from './components/BgParticles-file/BgParticles';
+import SectionFloat from './components/SectionFloat-file/SectionFloat';
+import HeroSection from './components/HeroSection-file/HeroSection';
 
 function App() {
   return (
-    <div >
-      <Navbar name={"Sebastian Aroena"} contact={"Contacto"} />
+    <div>
+      <BgParticles />
+
+      <Navbar name={"Sebastian Arocena Geldenhuys"} contact={"Contacto"} />
+      <container className="siteContainer">
+        <div className='mainContainer'>
+          <HeroSection title={"Hola Bienvenido"} mainDescriptionHero={"Mi nombre es Sebastian Arocena y mi novia es Valentina =)"}/>
+          <SectionFloat title={"soy Seba =)"} mainDescription={"Mi nombre es Sebastian Arocena y mi novia es Valentina =)"} />
+        </div>
+      </container>
+
+
     </div>
-
-
   );
 }
 
