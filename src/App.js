ee7bdc1529af6da3e8b9fb8b51c89bcff8a48e9c
sarocenaP <sarocena@polotab.com>
2025-06-15 00:09:10 -0300

    avance foto Hero IA

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
       <Navbar name={"Sebastian Arocena Geldenhuys"} contact={"Contacto"} />
       <container className="siteContainer">
         <div className='mainContainer'>
-          <HeroSection title={"Hola Bienvenido"} mainDescriptionHero={"Mi nombre es Sebastian Arocena y mi novia es Valentina =)"}/>
+          <HeroSection title={"Hola Bienvenido"} mainDescriptionHero={"Bienvenido a mi espacio digital. Soy Sebastian Arocena, Front-End Developer enfocado en crear interfaces modernas y funcionales."}/>
           <SectionFloat title={"soy Seba =)"} mainDescription={"Mi nombre es Sebastian Arocena y mi novia es Valentina =)"} />
         </div>
       </container>
