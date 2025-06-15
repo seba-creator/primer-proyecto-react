4ac9c25c9c95e9209cb4f8f610df80c3ecfb2733
sarocenaP <sarocena@polotab.com>
2025-06-15 00:13:26 -0300

    avance foto Hero y limpio codigo

--- src/components/HeroSection-file/HeroSection.jsx
+++ src/components/HeroSection-file/HeroSection.jsx
@@ -1,7 +1,5 @@
 import './HeroSection.css'
-import fotoPerfil from '../../assets/perfil.JPG';
-import fotoPerfil1 from '../../assets/FotoPortfolio.jpeg';
-import fotoPerfil2 from '../../assets/PortfolioPerfil.png';
+import fotoPerfilHero from '../../assets/PortfolioPerfil.png';
 
 
 function HeroSection({ title, mainDescriptionHero }) {
@@ -13,7 +11,7 @@ function HeroSection({ title, mainDescriptionHero }) {
                 <p className="pDescHero">{mainDescriptionHero}</p>
             </div>
             <div className='heroRight'>
-                <img src={fotoPerfil2} className='fotoPerfil' alt="Foto de perfil" />
+                <img src={fotoPerfilHero} className='fotoPerfil' alt="Foto de perfil" />
             </div>
 
         </section>
