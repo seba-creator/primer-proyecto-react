d8df4825d373197bc80ffcf2b3ac030b248072f8
seba-creator <sebasarocena2@gmail.com>
2025-06-15 00:09:10 -0300

    avance foto Hero IA

--- src/components/HeroSection-file/HeroSection.jsx
+++ src/components/HeroSection-file/HeroSection.jsx
@@ -1,15 +1,19 @@
 import './HeroSection.css'
+import fotoPerfil from '../../assets/perfil.JPG';
+import fotoPerfil1 from '../../assets/FotoPortfolio.jpeg';
+import fotoPerfil2 from '../../assets/PortfolioPerfil.png';
+
 
 function HeroSection({ title, mainDescriptionHero }) {
 
     return (
         <section className="heroContainer">
-            <div>
+            <div className='heroInfo'>
                 <h1>{title}</h1>
                 <p className="pDescHero">{mainDescriptionHero}</p>
             </div>
-            <div>
-                <p>BLA BLA BLA</p>
+            <div className='heroRight'>
+                <img src={fotoPerfil2} className='fotoPerfil' alt="Foto de perfil" />
             </div>
 
         </section>
